93f0dd9bc9921c04ed2b71f95996bcfd6f06c43b
Jeremy Cochoy <jeremy.cochoy@gmail.com>
2015-12-13 12:46:13 +0100

    rebuild lib

--- src/View.cpp
+++ src/View.cpp
@@ -31,11 +31,121 @@ void View::animate_particles()
         purple.push_back(Particle(41, 0, 0, 1, 'w'));
     }
 
-    if (time == 30)
+    if (time == 40)
     {
         orange.push_back(Particle(0, 0, 1, 1, '&'));
         orange.push_back(Particle(4, 0, 1, 1, '^'));
         orange.push_back(Particle(8, 0, 1, 1, '@'));
+    }
+
+    if (time == 63)
+    {
+        red.push_back(Particle(40, 0, -0.49, 1, '&'));
+        red.push_back(Particle(40, 0, +0.49, 1, '&'));
+
+    }
+
+    if (time == 80)
+    {
+        green.push_back(Particle(79, 0, -1, 1, '&'));
+        green.push_back(Particle(76, 0, -1, 1, '^'));
+        green.push_back(Particle(72, 0, -1, 1, '@'));
+    }
+
+    Particles::iterator it;
+    if (time > 100 && time < 400)
+    {
+        if (time % 10 == 0)
+            yellow.push_back(Particle(40, 0, 0, 1.5, '$'));
+    }
+    if (time < 430)
+        for (it = yellow.begin(); it != yellow.end(); it++)
+        {
+            float dx, dy;
+            if(time > 250)
+            {
+                dx = (7 - it->x);
+                dy = (40 - it->y);
+            }
+            else
+            {
+                dx = (80-7 - it->x);
+                dy = (40 - it->y);
+            }
+
+            it->vx = 0.03*dx;
+            it->vy = 0.03*dy;
+        }
+
+    if (time > 440 && time < 800)
+    {
+        if (time % 40 == 0)
+        {
+            orange.push_back(Particle(1,  0, 0, 0.3, '['));
+            orange.push_back(Particle(5,  0, 0, 0.3, '{'));
+            orange.push_back(Particle(10, 0, 0, 0.3, '('));
+
+            orange.push_back(Particle(79, 0, 0, 0.3, ']'));
+            orange.push_back(Particle(74, 0, 0, 0.3, '}'));
+            orange.push_back(Particle(70, 0, 0, 0.3, ')'));
+        }
+
+    }
+    if (time < 860)
+        for (it = orange.begin(); it != orange.end(); it++)
+            it->vx += 0.002*(40 - it->x);
+
+    if (time == 890)
+    {
+        red   .push_back(Particle( 1,  1, 0, 0, 'e'));
+        purple.push_back(Particle(70,  1, 0, 0, 'i'));
+        red   .push_back(Particle(20,  1, 0, 0, 'i'));
+        orange.push_back(Particle(10,  2, 0, 0, 'a'));
+        red   .push_back(Particle(79,  0, 0, 0.3, 'k'));
+        orange.push_back(Particle( 1,  3, 0, 0.3, 's'));
+        purple.push_back(Particle( 3,  2, 0, 0.3, 's'));
+        green .push_back(Particle(70,  4, 0, 0.3, 'l'));
+        purple.push_back(Particle( 7,  1, 0, 0, 'q'));
+        red   .push_back(Particle( 6,  2, 0, 0, 'r'));
+        green .push_back(Particle(50,  1, 0, 0, 'h'));
+        purple.push_back(Particle(40,  0, 0, 0, 'g'));
+        green .push_back(Particle(43,  6, 0, 0, 'u'));
+
+    }
+    if (time >= 890 && time < 1100)
+    {
+        for (it = orange.begin(); it != orange.end(); it++)
+        {
+            it->vx -= 0.02*(40 - it->x);
+            it->vy -= 0.02*(-3 - it->y);
+        }
+        for (it = red.begin(); it != red.end(); it++)
+        {
+            it->vx -= 0.02*(40 - it->x);
+            it->vy -= 0.02*(-3 - it->y);
+        }
+        for (it = green.begin(); it != green.end(); it++)
+        {
+            it->vx -= 0.02*(40 - it->x);
+            it->vy -= 0.02*(-3 - it->y);
+        }
+        for (it = purple.begin(); it != purple.end(); it++)
+        {
+            it->vx -= 0.02*(40 - it->x);
+            it->vy -= 0.02*(-3 - it->y);
+        }
+    }
+
+
+    if (time > 1100)
+    {
+        if (time % 10 == 0)
+        {
+            purple.push_back(Particle(time % 14 + 20, 0, 0, 1, '['));
+            orange.push_back(Particle(time % 13, 3, 0, 1, '}'));
+            green.push_back(Particle(time % 14 + 45, 9, 0, 1, ')'));
+            yellow.push_back(Particle(time % 4 + 75, 0, 0, 1, ';'));
+        }
     }
 
     /* Animation */
@@ -94,7 +204,7 @@ void View::reattach_particles(Particles &particles)
     while (it != particles.end())
     {
         if (std::abs(it->x - player.x) < player.size
-            && std::abs(it->y - 15) < player.size)
+            && std::abs(it->y - 23) < player.size)
         {
             player.particles.push_back(*it);
             it = particles.erase(it);
@@ -112,7 +222,7 @@ bool View::refresh()
     if (w < 80 || h < 30)
     {
         mlc_clear();
-        std::string msg = "Smallest size : 80x60";
+        std::string msg = "Smallest size : 80x30";
 
         int x = x/2 - msg.length()/2;
         if (x < 0)
@@ -141,6 +251,16 @@ bool View::refresh()
     player.draw();
 
     mlc_show();
+
+    if (player.particles.size() >= 100)
+    {
+        mlc_clear();
+        std::cout << "Congratulation, you finished the game, with "
+                  << player.particles.size() << " letters, after "
+                  << time << " processor tics. Why not take a bit of sun?"
+                  << std::endl;
+        return false;
+    }
 
     return run;
 }
@@ -177,7 +297,7 @@ float sign(float v)
 
 void Player::draw()
 {
-    mlc_setpos(x, 15);
+    mlc_setpos(x, 23);
     mlc_putcolor(CLR__BLACK);
     mlc_putcolor(CLR_RED);
     mlc_putchar('O');
@@ -188,16 +308,19 @@ void Player::draw()
         it->x += it->vx;
         it->y += it->vy;
         it->vx += (x - it->x) / 6.0f;
-        it->vy += (15 - it->y) / 6.0f;
+        it->vy += (23 - it->y) / 6.0f;
         if (std::abs(it->x - x) > size)
             it->vx -= 0.3 * it->vx;
-        if (std::abs(it->y - 15) > size)
+        if (std::abs(it->y - 23) > size)
             it->vy -= 0.3 * it->vy;
-        mlc_setpos((int)it->x, (int)it->y);
-        mlc_putcolor(CLR_BLUE);
-        mlc_putchar(it->c);
+        if (it->x >0 && it->y > 0 && it->x <80 && it->y < 30)
+        {
+            mlc_setpos((int)it->x, (int)it->y);
+            mlc_putcolor(CLR_BLUE);
+            mlc_putchar(it->c);
+        }
     }
 
 
-    size += 0.001 * 1 / size;
+    size = 4 + particles.size()/10.0f;
 }
